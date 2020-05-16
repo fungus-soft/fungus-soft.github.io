fa4f5030621da74bd2f05ec549c3e773e09b7210
IsaiahPatton <isaiahpatton1@gmail.com>
2020-05-16 01:44:44 -0400

    basic commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("body").style.minHeight = (window.innerHeight - document.getElementById("header").style.height - document.getElementById("footer").style.height) + "px";
+
+function sizechange() {
+    if (window.innerWidth < 600)
+         document.getElementById("h1").style.fontSize = "28px";
+    else document.getElementById("h1").style.fontSize = "36px";
+}
+window.onresize = sizechange;
+sizechange();
