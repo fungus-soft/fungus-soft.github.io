494925685af4ed8d7e1fb644246140c9bd4094ee
IsaiahPatton <isaiahpatton1@gmail.com>
2020-06-29 00:56:07 -0400

    SpeedyAds on all subpages

---
+++ assets/js/side.js
@@ -0,0 +1,22 @@
+function sideAd() {
+  sa_client = "a859de98a47a2fd0091878cd36b45c4f";
+  sa_code = "2e0a0e48af104d8c6c693a59ed910199";
+  sa_protocol = ("https:"==document.location.protocol)?"https":"http";
+  sa_pline = "0";
+  sa_maxads = "4";
+  sa_bgcolor = "ffffff";
+  sa_bordercolor = "bdd631";
+  sa_superbordercolor = "bdd631";
+  sa_linkcolor = "001eb5";
+  sa_desccolor = "000000";
+  sa_urlcolor = "788300";
+  sa_b = "1";
+  sa_format = "column_120x600";
+  sa_width = "120";
+  sa_height = "600";
+  sa_location = "0";
+  sa_radius = "0";
+  sa_borderwidth = "1";
+  sa_font = "0";
+  document.write(unescape("%3cscript type='text/javascript' src='"+sa_protocol+"://sa.entireweb.com/sense2.js'%3e%3c/script%3e"));
+}
